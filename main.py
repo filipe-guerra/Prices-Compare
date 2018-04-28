cd7484f8d72b4eded95bc920f723da584300cb65
filipe-guerra <filipehmguerra@yahoo.com.br>
2018-04-28 17:42:52 -0300

    3 Exchanges and format adopted

--- main.py
+++ main.py
@@ -15,13 +15,93 @@ from BeautifulSoup import *
 class exBinance():
     def __init__(self):
         self.url = 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT'
-    
+        self.bid = ''
+        self.ask = ''
+        
     def getPrice(self):
         try:
             uh = urllib.urlopen(self.url)
             data = uh.read()
             
             info = json.loads(data)
-            return info['price']
+            aux = float(info['price'])
+
+            return format(aux, '.2f')
         except:
             return '#'
+    
+    def getBid(self):
+        try:
+            uh = urllib.urlopen('https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCUSDT')
+            data = uh.read()
+            
+            info = json.loads(data)
+            bidP = float(info['bidPrice'])
+            self.bid = format(bidP, '.2f')
+            
+            askP = float(info['askPrice'])
+            self.ask = format(askP, '.2f')
+
+            return self.bid
+        except:
+            return '#'
+    
+    def getAsk(self):
+        return self.ask
+            
+class exKucoin():
+    def __init__(self):
+        self.url = 'https://api.kucoin.com/v1/BTC-USDT/open/tick'
+        self.bid = ''
+        self.ask = ''
+        
+    def getPrice(self):
+        try:
+            uh = urllib.urlopen(self.url)
+            data = uh.read()
+            
+            info = json.loads(data)
+            aux = format((info['data']['lastDealPrice']), '.2f')
+            
+            self.bid = format((info['data']['buy']), '.2f')
+            self.ask = format((info['data']['sell']), '.2f')
+            
+            return aux
+        except:
+            return '#'
+            
+    def getBid(self):
+        return self.bid
+    
+    def getAsk(self):
+        return self.ask
+            
+class exBitfinex():
+    def __init__(self):
+        self.url = 'https://api.bitfinex.com/v2/ticker/tBTCUSD'
+        self.bid = ''
+        self.ask = ''
+        
+    def getPrice(self):
+        try:
+            uh = urllib.urlopen(self.url)
+            data = uh.read()
+           
+            info = json.loads(data)
+            aux = format(info[6], '.2f')
+            
+            self.bid = format(info[0], '.2f')
+            self.ask = format(info[2], '.2f')
+            
+            # Bitfinex possui apenas 1 casa decimal
+            return aux
+        except:
+            return '#'
+            
+    def getBid(self):
+        return self.bid
+    
+    def getAsk(self):
+        return self.ask
+            
+            
--- server.py
+++ server.py
@@ -14,6 +14,10 @@ app = dash.Dash()
 
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
+binance = exBinance()
+kucoin = exKucoin()
+bitfinex = exBitfinex()
+
 def generate_table(dataframe, max_rows=10):
     return html.Table(
         # Header
@@ -32,26 +36,34 @@ def generate_table2():
         # Header
         [html.Tr([
             html.Th("Exchange"),
-            html.Th("Price"),
-            html.Th("Bid"),
-            html.Th("Ask"),
+            html.Th("Price (U$)"),
+            html.Th("Bid (U$)"),
+            html.Th("Ask (U$)"),
             ]
             )] +
 
         # Body
         [html.Tr([
             html.Td("Binance"),
-            html.Td(exBinance().getPrice(), id="bin-price"),
-            html.Td("BinanceBid"),
-            html.Td("BinanceAsk"),
+            html.Td(binance.getPrice(), id="bin-price", style={"color":"blue"}),
+            html.Td(binance.getBid(), id="binbid", style={"color":"blue"}),
+            html.Td(binance.getAsk(), id="binask", style={"color":"blue"}),
             ]
         )] + 
         
         [html.Tr([
             html.Td("Kucoin"),
-            html.Td("price"),
-            html.Td("Bid"),
-            html.Td("Ask"),
+            html.Td(kucoin.getPrice(), id="kuk-price", style={"color":"blue"}),
+            html.Td(kucoin.getBid(), id="kukbid", style={"color":"blue"}),
+            html.Td(kucoin.getAsk(), id="kukask", style={"color":"blue"}),
+            ]
+        )] +
+            
+        [html.Tr([
+            html.Td("Bitfinex"),
+            html.Td(bitfinex.getPrice(), id="btf-price", style={"color":"blue"}),
+            html.Td(bitfinex.getBid(), id="btfbid", style={"color":"blue"}),
+            html.Td(bitfinex.getAsk(), id="btfask", style={"color":"blue"}),
             ]
         )]
     
@@ -73,19 +85,83 @@ app.layout = html.Div(children=[
     
     dcc.Interval(
         id='field-update', 
-        interval=5000
+        interval=7000
         ),
     
     #generate_table(df),
 ])
 
+#   BINANCE
+
 @app.callback(
     Output('bin-price', component_property='children'),
     events = [Event('field-update', 'interval')]
 )
-def update_output_div():
-    price = exBinance().getPrice()
-    return 'U$ {}'.format(price)
+def update_bin():
+    return binance.getPrice()
+
+@app.callback(
+    Output('binbid', component_property='children'),
+    events = [Event('field-update', 'interval')]
+)
+def update_binbid():
+   return binance.getBid()
+
+@app.callback(
+    Output('binask', component_property='children'),
+    events = [Event('field-update', 'interval')]
+)
+def update_binask():
+    return binance.getAsk()
+
+
+#   KUCOIN
+
+@app.callback(
+    Output('kuk-price', component_property='children'),
+    events = [Event('field-update', 'interval')]
+)
+def update_kuk():
+    return kucoin.getPrice()
+ 
+@app.callback(
+    Output('kukbid', component_property='children'),
+    events = [Event('field-update', 'interval')]
+)
+def update_kukbid():
+    return kucoin.getBid()
+
+ 
+@app.callback(
+    Output('kukask', component_property='children'),
+    events = [Event('field-update', 'interval')]
+)
+def update_kukask():
+    return kucoin.getAsk()
+ 
+ # BITFINEX 
+   
+@app.callback(
+    Output('btf-price', component_property='children'),
+    events = [Event('field-update', 'interval')]
+)
+def update_btf():
+    return bitfinex.getPrice()
+   
+@app.callback(
+    Output('btfbid', component_property='children'),
+    events = [Event('field-update', 'interval')]
+)
+def update_btfbid():
+    return bitfinex.getBid()
+   
+@app.callback(
+    Output('btfask', component_property='children'),
+    events = [Event('field-update', 'interval')]
+)
+def update_btfask():
+    return bitfinex.getAsk()
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
